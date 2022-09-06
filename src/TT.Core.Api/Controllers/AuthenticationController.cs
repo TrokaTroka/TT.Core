731c5f7d3ff8685829f1e9652060325c7ecabee5
Roberio <roberio.pb@hotmail.com>
2022-09-06 13:40:29 -0300

    Api

--- src/TT.Core.Api/Controllers/AuthenticationController.cs
+++ src/TT.Core.Api/Controllers/AuthenticationController.cs
@@ -18,7 +18,7 @@ public class AuthenticationController : ControllerBase
     [Route("check")]
     public IActionResult Check()
     {
-        return new JsonResult("Api ok");
+        return new JsonResult("Api okay");
     }
 
     [HttpPost]
